9a6c317deb6c4489196d5e0a9905a08e995684df
Breanna Higgins <bbhiggins520@outlook.com>
2017-07-02 18:56:01 -0400

    Text changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # simpleDataStructures
+TODO
--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <msp430.h> 
 
 /**
- * @file
+ * @file main.c
  * @group demo
  * @mainpage
  * @author Breanna Higgins
  *      Created 7/2/2017
- * @brief This main function is to test the Simple Data Structures Library. It demostrates the usage of
- * main.c
+ * @brief This main function is to test the Simple Data Structures Library.
+ *
+ * </br>
+ * It demonstrates the usage of the library for applications in embedded systems. It is tested on the MSP430f5529.
+ * Current includes these structures:
+ *  - NONE
+ * The following structures are in development:
+ *  - List
+ * The following structures are yet to be completed:
+ *  - Linked List
+ *  - Stack
+ *  - Queue
+ *  - Double Ended Queue
+ *  - Circular Queue
+ *  - Circular DE Queue
+ *  - Heap
+ *  - Binary Search Tree
+ *
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
